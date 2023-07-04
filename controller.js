bee2cf58576f8fcf34c643990c971c59632ee5b7
Evandra Raditya <evandraraditya049@gmail.com>
2023-07-04 14:25:36 +0700

    refactor: 🎨 Change the try catch position

--- controller.js
+++ controller.js
@@ -1,9 +1,8 @@
 const sendMessageGet = async (req, res, client) => {
-  const { number, message } = req.query;
-
-  const number_details = await client.getNumberId(number);
-
   try {
+    const { number, message } = req.query;
+
+    const number_details = await client.getNumberId(number);
     client.sendMessage(number_details._serialized, message);
 
     res.send("Message sent!");
@@ -13,11 +12,10 @@ const sendMessageGet = async (req, res, client) => {
 };
 
 const sendMessagePost = async (req, res, client) => {
-  const { number, message } = req.body;
-
-  const number_details = await client.getNumberId(number);
-
   try {
+    const { number, message } = req.body;
+
+    const number_details = await client.getNumberId(number);
     client.sendMessage(number_details._serialized, message);
 
     res.send("Message sent!");
